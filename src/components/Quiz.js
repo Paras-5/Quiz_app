e4056ff1e78fb48d121106bd50b606bb9437b019
Paras-5 <paras.ag94@gmail.com>
2022-01-14 17:07:42 +0530

    app_structure

---
+++ src/components/Quiz.js
@@ -0,0 +1,17 @@
+import React from "react"
+import "../css/style.css"
+import Quizdata from "./Quizdata"
+
+function Quiz() {
+    return (
+        <>
+        <div className="navbar">
+        <span>View Highscore </span><i className="fas fa-hand-point-left fa-lg"></i>
+        </div>
+        <div className="Quesbox">
+        </div>
+        </>
+    )
+}
+
+export default Quiz
